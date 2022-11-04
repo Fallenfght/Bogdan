29a906d5b48ce520b198d382942932cd8b262090
Fallenfght <bogdan-3098@mail.ru>
2022-11-04 17:36:03 +0600

    js

--- b24.kz/js/script.js
+++ b24.kz/js/script.js
@@ -412,14 +412,17 @@ if (modal && buttonModal.length) {
 		element.addEventListener('click', function(e) {
 			e.preventDefault();
 			modal.classList.add('_active');
+			bodyFixPosition();
 		})
 	}
 	modal.addEventListener('click', function() {
 		modal.classList.remove('_active');
+		bodyUnfixPosition();
 	})
 	const modalClose = modal.querySelector('.modal__close');
 	modalClose.addEventListener('click', function(e) {
 		modal.classList.remove('_active');
+		bodyUnfixPosition();
 	})
 	const modalContainer = modal.querySelector('.modal__container');
 	modalContainer.addEventListener('click', function(e) {
@@ -432,4 +435,52 @@ if (masks.length) {
 		mask: "+7 (999) 999 99 99",
 		showMaskOnHover: false,
 	}).mask(masks);
+}
+// 1. Фиксация <body>
+function bodyFixPosition() {
+
+  setTimeout( function() {
+  /* Ставим необходимую задержку, чтобы не было «конфликта» в случае, если функция фиксации вызывается сразу после расфиксации (расфиксация отменяет действия расфиксации из-за одновременного действия) */
+
+    if ( !document.body.hasAttribute('data-body-scroll-fix') ) {
+
+      // Получаем позицию прокрутки
+      let scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+
+      // Ставим нужные стили
+      document.body.setAttribute('data-body-scroll-fix', scrollPosition); // Cтавим атрибут со значением прокрутки
+      document.body.style.overflow = 'hidden';
+      document.body.style.position = 'fixed';
+      document.body.style.top = '-' + scrollPosition + 'px';
+      document.body.style.left = '0';
+      document.body.style.width = '100%';
+
+    }
+
+  }, 15 ); /* Можно задержку ещё меньше, но у меня работало хорошо именно с этим значением на всех устройствах и браузерах */
+
+}
+// 2. Расфиксация <body>
+function bodyUnfixPosition() {
+
+  if ( document.body.hasAttribute('data-body-scroll-fix') ) {
+
+    // Получаем позицию прокрутки из атрибута
+    let scrollPosition = document.body.getAttribute('data-body-scroll-fix');
+
+    // Удаляем атрибут
+    document.body.removeAttribute('data-body-scroll-fix');
+
+    // Удаляем ненужные стили
+    document.body.style.overflow = '';
+    document.body.style.position = '';
+    document.body.style.top = '';
+    document.body.style.left = '';
+    document.body.style.width = '';
+
+    // Прокручиваем страницу на полученное из атрибута значение
+    window.scroll(0, scrollPosition);
+
+  }
+
 }
