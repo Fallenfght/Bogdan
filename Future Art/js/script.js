6d9fc04ff8aca541c724a3c253a98fe19caeed03
Fallenfght <bogdan-3098@mail.ru>
2022-11-18 15:41:05 +0600

    future art

---
+++ Future Art/js/script.js
@@ -0,0 +1,80 @@
+/*document.addEventListener('DOMContentLoaded', function () {
+	let animateItems = document.querySelectorAll("[class*='animate-']");
+	let animateItemsArray = Array.prototype.slice.call(animateItems);
+	let locked = false;
+	if (animateItemsArray.length > 0) {
+		window.addEventListener('scroll', animations);
+		function animations() {
+			if (locked) return;
+			locked = true;
+			setTimeout(() => {
+				for (let index = 0; index < animateItemsArray.length; index++) {
+					let element = animateItemsArray[index];
+					let elemPos = element.getBoundingClientRect().top - window.innerHeight / 2;
+					if (elemPos <= 0) {
+						element.classList.add('_active');
+						animateItemsArray.splice(index, 1);
+						index = index - 1;
+					}
+				}
+				if (animateItemsArray == 0) {
+					window.removeEventListener('scroll', animations);
+				}
+				locked = false;
+			}, 100)
+		}
+	}
+});*/
+
+/*// 1. Фиксация <body>
+function bodyFixPosition() {
+	setTimeout(function() {
+		if (!document.body.hasAttribute('data-body-scroll-fix')) {
+			let scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+			document.body.setAttribute('data-body-scroll-fix', scrollPosition);
+			document.body.style.overflow = 'hidden';
+			document.body.style.position = 'fixed';
+			document.body.style.top = '-' + scrollPosition + 'px';
+			document.body.style.left = '0';
+		}
+	}, 15);
+}
+function bodyUnfixPosition() {
+	if (document.body.hasAttribute('data-body-scroll-fix')) {
+		let scrollPosition = document.body.getAttribute('data-body-scroll-fix');
+		document.body.removeAttribute('data-body-scroll-fix');
+		document.body.style.overflow = '';
+		document.body.style.position = '';
+		document.body.style.top = '';
+		document.body.style.left = '';
+		window.scroll(0, scrollPosition);
+	}
+}
+//===============================*/
+document.addEventListener('DOMContentLoaded', function () {
+	let animateItems = document.querySelectorAll("[class*='animate-']");
+	let animateItemsArray = Array.prototype.slice.call(animateItems);
+	let locked = false;
+	if (animateItemsArray.length > 0) {
+		window.addEventListener('scroll', animations);
+		function animations() {
+			if (locked) return;
+			locked = true;
+			setTimeout(() => {
+				for (let index = 0; index < animateItemsArray.length; index++) {
+					let element = animateItemsArray[index];
+					let elemPos = element.getBoundingClientRect().top - window.innerHeight / 2;
+					if (elemPos <= 0) {
+						element.classList.add('_active');
+						animateItemsArray.splice(index, 1);
+						index = index - 1;
+					}
+				}
+				if (animateItemsArray == 0) {
+					window.removeEventListener('scroll', animations);
+				}
+				locked = false;
+			}, 100)
+		}
+	}
+});
